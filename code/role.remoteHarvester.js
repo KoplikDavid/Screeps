df50dcfce7abcbded81195a479fedb9b632860c6
Koplik David <koplikdavid@gmail.com>
2020-02-16 14:35:54 +0100

    snad

--- code/role.remoteHarvester.js
+++ code/role.remoteHarvester.js
@@ -1,30 +1,31 @@
 var roleRemoteHarvester = {
 
-    /** @param {Creep} creep **/
-    run: function(creep) {
-	    if(creep.store.getFreeCapacity() == 0) {
-        var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return structure.structureType == STRUCTURE_CONTAINER;
-                }
-        });
-
-        if(targets.length > 0) {
-          target = creep.pos.findClosestByPath(targets);
-            if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-            }
+  /** @param {Creep} creep **/
+  run: function(creep) {
+    if(creep.store.getFreeCapacity() > 0) {
+          var sources = creep.room.find(FIND_SOURCES);
+          if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
           }
+      }
+      else {
+          var targets = creep.room.find(FIND_STRUCTURES, {
+                  filter: (structure) => {
+                      return (structure.structureType == STRUCTURE_EXTENSION ||
+                          structure.structureType == STRUCTURE_SPAWN ||
+                          structure.structureType == STRUCTURE_CONTAINER ||
+                          structure.structureType == STRUCTURE_TOWER) &&
 
-
-        else {
-          if(creep.store.getFreeCapacity() > 0) {
-                var sources = creep.room.find(FIND_SOURCES);
-                if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-                }
+                          structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                  }
+          });
+          if(targets.length > 0) {
+            target = creep.pos.findClosestByPath(targets);
+              if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                  creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+              }
           }
-	       }
-       }
+      }
+}
 };
 module.exports = roleRemoteHarvester;
