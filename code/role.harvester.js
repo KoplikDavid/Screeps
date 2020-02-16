0c038a2f7762a4ac459c1d534d18a5455075e85b
Koplik David <koplikdavid@gmail.com>
2020-02-16 16:04:55 +0100

    format

--- code/role.harvester.js
+++ code/role.harvester.js
@@ -3,9 +3,9 @@ var roleHarvester = {
     /** @param {Creep} creep **/
   run: function (creep) {
 	    if (creep.store.getFreeCapacity() > 0) {
-      var sources = creep.room.find(FIND_SOURCES)
+      var sources = creep.room.find(FIND_SOURCES);
       if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}})
+        creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
       }
     } else {
       var targets = creep.room.find(FIND_STRUCTURES, {
@@ -15,17 +15,17 @@ var roleHarvester = {
                             structure.structureType == STRUCTURE_CONTAINER ||
                             structure.structureType == STRUCTURE_TOWER) &&
 
-                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
         }
-      })
+      });
       if (targets.length > 0) {
-        target = creep.pos.findClosestByPath(targets)
+        target = creep.pos.findClosestByPath(targets);
         if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}})
+          creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
         }
       }
     }
   }
-}
+};
 
-module.exports = roleHarvester
+module.exports = roleHarvester;
