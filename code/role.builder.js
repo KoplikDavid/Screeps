df65a945b9261811979ac537033503e9985a2e8f
Koplik David <koplikdavid@gmail.com>
2020-02-16 16:04:55 +0100

    format

--- code/role.builder.js
+++ code/role.builder.js
@@ -3,36 +3,36 @@ var roleBuilder = {
     /** @param {Creep} creep **/
   run: function (creep) {
 	    if (creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
-      creep.memory.building = false
-      creep.memory.haveSourse = true
-      creep.say('🔄 harvest')
+      creep.memory.building = false;
+      creep.memory.haveSourse = true;
+      creep.say('🔄 harvest');
 	    }
 	    if (!creep.memory.building && creep.store.getFreeCapacity() == 0) {
-	        creep.memory.building = true
-      creep.memory.haveSourse = false
-	        creep.say('🚧 build')
+	        creep.memory.building = true;
+      creep.memory.haveSourse = false;
+	        creep.say('🚧 build');
 	    }
 
 	    if (creep.memory.building) {
-	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES)
+	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
       if (targets.length) {
-        let target = creep.pos.findClosestByPath(targets)
+        let target = creep.pos.findClosestByPath(targets);
         if (creep.build(target) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}})
+          creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
         }
       }
 	    } else {
       if (creep.memory.haveSourse == false) {
-        var sources = creep.room.find(FIND_SOURCES)
-        creep.memory.source = creep.pos.findClosestByPath(sources).id
-        creep.memory.haveSourse = true
+        var sources = creep.room.find(FIND_SOURCES);
+        creep.memory.source = creep.pos.findClosestByPath(sources).id;
+        creep.memory.haveSourse = true;
       }
 
       if (creep.harvest(creep.memory.source) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(creep.memory.source, {visualizePathStyle: {stroke: '#ffaa00'}})
+        creep.moveTo(creep.memory.source, {visualizePathStyle: {stroke: '#ffaa00'}});
       }
 	    }
   }
-}
+};
 
-module.exports = roleBuilder
+module.exports = roleBuilder;
